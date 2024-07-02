981f20422d21f9d9ea1ca73b54c32a7bcc24fd4f
Marghubur <marghub12@gmail.com>
2024-07-02 18:43:02 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogCardComponent } from './blog-card/blog-card.component';
+import { BookCardComponent } from './book-card/book-card.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { IndexComponent } from './index/index.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VideosComponent } from './videos/videos.component';
+import { ViewBookComponent } from './view-book/view-book.component';
+import { ViewPostComponent } from './view-post/view-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogCardComponent,
+    BookCardComponent,
+    BookListComponent,
+    FooterComponent,
+    HomeComponent,
+    IndexComponent,
+    NavbarComponent,
+    VideosComponent,
+    ViewBookComponent,
+    ViewPostComponent
   ],
   imports: [
     BrowserModule,
